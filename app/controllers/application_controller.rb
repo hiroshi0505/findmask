7809182654ccb1c53c4277bf6cc63c69d72cc1c0
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-25 19:39:16 +0900

    登録時にnicknameのバリデーション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! # ログインしていないユーザーをログインページの画面に促す
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
