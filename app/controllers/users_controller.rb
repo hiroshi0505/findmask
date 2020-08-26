890e26c2c76993c48d9f1f3e585eb0282299f073
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-26 14:09:55 +0900

    pagination導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets
+    @tweets = user.tweets.paginate(page: params[:page], per_page: 1)
   end
 end
