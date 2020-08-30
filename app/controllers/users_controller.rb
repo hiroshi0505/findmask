9cbdd5db049bdccc0e5656e36e8de1f37ad67908
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-30 19:59:32 +0900

    マイページの投稿一覧を降順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets.paginate(page: params[:page], per_page: 1)
+    @tweets = user.tweets.paginate(page: params[:page], per_page: 1).order("created_at DESC") # ASC（昇順）
   end
 end
