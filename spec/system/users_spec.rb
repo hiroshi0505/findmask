45c307b3dd00a5f2c5d09178bda872085d17e05a
hiroshi0505 <a1500095@hotmail.co.jp>
2020-09-06 15:56:17 +0900

    新規登録の結合テスト追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,12 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe "ユーザーログイン機能", type: :system do
-  it 'ログインしていない場合、サインページに移動する' do
+RSpec.describe "ユーザー管理機能", type: :system do
+  it 'ログインしていない場合、サインインページに移動する' do
     # トップページに遷移する
     visit root_path
 
     # ログインしていない場合、サインインページに遷移していることを確認する
+    # expect(current_path).to eq "/users/sign_in"
+      expect(current_path).to eq new_user_session_path
+  end
+
+  it '新規登録に成功し、トップページに遷移する' do
+    # サインインページへ移動する
+    visit  new_user_session_path
+
+    # ログインしていない場合、サインインページに遷移していることを確認する
     expect(current_path).to eq new_user_session_path
+
+    # 新規登録ボタンをクリックする
+    click_on("新規登録")
+
+    # すでに保存されているユーザーのemailとpasswordを入力する
+    fill_in 'user_nickname', with: "aaa"
+    fill_in 'user_email', with: "a@a"
+    fill_in 'user_password', with: "a1500095"
+    fill_in 'user_password_confirmation', with: "a1500095"
+
+    # ログインボタンをクリックする
+    click_on("Sign up")
+
+    # トップページに遷移していることを確認する
+    expect(current_path).to eq root_path
+  end
+
+  it '新規登録に失敗し、再び新規登録ページに遷移する' do
+    # サインインページへ移動する
+    visit  new_user_session_path
+
+    # ログインしていない場合、サインインページに遷移していることを確認する
+    expect(current_path).to eq new_user_session_path
+
+    # 新規登録ボタンをクリックする
+    click_on("新規登録")
+
+    # すでに保存されているユーザーのemailとpasswordを入力する
+    fill_in 'user_nickname', with: "aaa"
+    fill_in 'user_email', with: "a@a"
+    fill_in 'user_password', with: "a1500095"
+    fill_in 'user_password_confirmation', with: "test"
+
+    # ログインボタンをクリックする
+    click_on("Sign up")
+
+    # 新規登録ページに戻ってきていることを確認する
+    expect(current_path).to eq "/users"
   end
 
   it 'ログインに成功し、トップページに遷移する' do
@@ -30,7 +77,7 @@ RSpec.describe "ユーザーログイン機能", type: :system do
     expect(current_path).to eq root_path
   end
   
-  it 'ログインに失敗し、再びサインインページに戻ってくる' do
+  it 'ログインに失敗し、再びサインインページに遷移する' do
     # 予め、ユーザーをDBに保存する
     @user = FactoryBot.create(:user)
 
